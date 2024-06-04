4f5524069a6d662a32d3806e9955a35113fed4e7
t2ance <12111327@mail.sustech.edu.cn>
2024-06-04 11:29:02 -0700

    updated

--- config.py
+++ config.py
@@ -226,10 +226,10 @@ def get_config(system_=None, n_iteration=None):
                                    do_test=True)
     elif system_ == 's4':
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory', delay=1,
-                                       duration=8, dt=0.05, n_dataset=100, n_sample_per_dataset=-1, n_plot_sample=20,
+                                       duration=8, dt=0.125, n_dataset=200, n_sample_per_dataset=-1, n_plot_sample=20,
                                        ic_lower_bound=-2, ic_upper_bound=2, successive_approximation_n_iteration=10)
         model_config = ModelConfig(model_name='FNO', fno_n_layers=5, fno_n_modes_height=32, fno_hidden_channels=64)
-        train_config = TrainConfig(learning_rate=1e-3, training_ratio=0.8, n_epoch=250, batch_size=64,
+        train_config = TrainConfig(learning_rate=1e-3, training_ratio=0.8, n_epoch=250, batch_size=128,
                                    weight_decay=1e-2, log_step=-1, lr_scheduler_type='exponential',
                                    scheduler_gamma=0.97, scheduler_step_size=1, scheduler_min_lr=1e-5, debug=False,
                                    do_test=True)
