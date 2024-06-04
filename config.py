5ab0a3c77f711f86a39e15f2fd7b1fac08546366
t2ance <12111327@mail.sustech.edu.cn>
2024-06-04 14:10:22 -0700

    updated

--- config.py
+++ config.py
@@ -227,7 +227,7 @@ def get_config(system_=None, n_iteration=None):
         model_config = ModelConfig(model_name='FNO', fno_n_layers=5, fno_n_modes_height=32, fno_hidden_channels=64)
     elif system_ == 's3':
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory', delay=1,
-                                       duration=8, dt=0.05, n_dataset=400, n_sample_per_dataset=-1, n_plot_sample=20,
+                                       duration=8, dt=0.01, n_dataset=500, n_sample_per_dataset=-1, n_plot_sample=20,
                                        ic_lower_bound=-1, ic_upper_bound=1, successive_approximation_n_iteration=10)
         model_config = ModelConfig(model_name='FNO', fno_n_layers=6, fno_n_modes_height=32, fno_hidden_channels=64)
         train_config = TrainConfig(learning_rate=1e-3, training_ratio=0.8, n_epoch=250, batch_size=128,
