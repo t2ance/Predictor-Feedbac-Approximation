e94b7132a918fa881b09a67208df2b573abeb743
t2ance <12111327@mail.sustech.edu.cn>
2024-07-08 23:35:59 -0700

    updated

--- config.py
+++ config.py
@@ -311,11 +311,11 @@ def get_config(system_, n_iteration=None, duration=None, delay=None):
                                    scheduled_sampling_k=1e-2)
     elif system_ == 's5':
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory',
-                                       delay=1, duration=8, dt=0.05, n_dataset=200, n_sample_per_dataset=-1,
+                                       delay=0.5, duration=12, dt=0.05, n_dataset=200, n_sample_per_dataset=-1,
                                        n_plot_sample=20, ic_lower_bound=-0.5, ic_upper_bound=0.5,
                                        successive_approximation_n_iteration=5)
         model_config = ModelConfig(model_name='FNO', n_layer=5, fno_n_modes_height=32, fno_hidden_channels=64)
-        train_config = TrainConfig(learning_rate=1e-3, training_ratio=0.8, n_epoch=1000, batch_size=128,
+        train_config = TrainConfig(learning_rate=1e-3, training_ratio=0.8, n_epoch=3000, batch_size=64,
                                    weight_decay=1e-4, log_step=-1, lr_scheduler_type='exponential', alpha=0.01,
                                    scheduled_sampling_warm_start=0, load_model=False, do_test=False,
                                    scheduled_sampling_type='linear', scheduled_sampling_k=1e-2)
