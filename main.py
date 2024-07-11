7a72ce190ed7b2b933732ab910b8c24fc7d79cc3
t2ance <12111327@mail.sustech.edu.cn>
2024-07-11 12:25:30 -0700

    updated

--- main.py
+++ main.py
@@ -954,7 +954,8 @@ if __name__ == '__main__':
     train_config.training_type = args.training_type
     if args.training_type == 'offline':
         train_config.lr_scheduler_type = 'exponential'
-        train_config.n_epoch = 300
+        # train_config.n_epoch = 300
+        train_config.n_epoch = 0
     elif args.training_type == 'scheduled sampling':
         train_config.lr_scheduler_type = 'none'
         train_config.n_epoch = 2000
