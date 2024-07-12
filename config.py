d9c703f7826a75205fcf8dfb8f91d284ec28f941
t2ance <12111327@mail.sustech.edu.cn>
2024-07-11 20:13:56 -0700

    updated

--- config.py
+++ config.py
@@ -319,7 +319,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None):
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory',
                                        delay=0.5, duration=32, dt=0.025, n_dataset=200, n_sample_per_dataset=-1,
                                        n_plot_sample=20, ic_lower_bound=-0.2, ic_upper_bound=0.2,
-                                       integral_method='successive adaptive',
+                                       integral_method='successive adaptive'
                                        # integral_method='successive',
                                        # successive_approximation_n_iteration=5
                                        )
