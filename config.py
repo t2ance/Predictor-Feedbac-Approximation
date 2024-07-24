46aef65fd8563481b833841e50ca1a6787c49e17
t2ance <12111327@mail.sustech.edu.cn>
2024-07-24 08:19:44 -0700

    updated

--- config.py
+++ config.py
@@ -275,14 +275,14 @@ class DatasetConfig:
 
 def get_config(system_, n_iteration=None, duration=None, delay=None):
     if system_ == 's1':
-        dataset_config = DatasetConfig(recreate_training_dataset=False, data_generation_strategy='trajectory', delay=1,
-                                       duration=8, dt=0.05, n_dataset=1000, n_sample_per_dataset=-1, n_plot_sample=20,
+        dataset_config = DatasetConfig(recreate_training_dataset=False, data_generation_strategy='trajectory', delay=3,
+                                       duration=8, dt=0.05, n_dataset=250, n_sample_per_dataset=-1, n_plot_sample=20,
                                        integral_method='successive adaptive', random_test=True,
                                        ic_lower_bound=0, ic_upper_bound=1,
                                        random_test_lower_bound=0, random_test_upper_bound=1
                                        )
         model_config = ModelConfig(model_name='FNO', n_layer=5, fno_n_modes_height=32, fno_hidden_channels=32)
-        train_config = TrainConfig(learning_rate=1e-2, training_ratio=0.8, n_epoch=700, batch_size=64,
+        train_config = TrainConfig(learning_rate=1e-4, training_ratio=0.8, n_epoch=700, batch_size=64,
                                    do_training=True, do_testing=True, load_model=False,
                                    weight_decay=1e-3, log_step=-1, lr_scheduler_type='exponential',
                                    scheduler_gamma=0.97, scheduler_step_size=1, scheduler_min_lr=1e-6)
