800cdcd34f1586202d9a9381f33b54297049b3db
t2ance <12111327@mail.sustech.edu.cn>
2024-08-05 15:28:51 -0700

    updated

--- config.py
+++ config.py
@@ -343,7 +343,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
                                    scheduled_sampling_k=1e-2)
     elif system_ == 's7':
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory',
-                                       delay=TimeVaryingDelay(), duration=32, dt=0.01, n_dataset=10,
+                                       delay=TimeVaryingDelay(), duration=32, dt=0.01, n_dataset=250,
                                        n_sample_per_dataset=-1, ic_lower_bound=-0.5,
                                        ic_upper_bound=0.5)
         model_config = ModelConfig(model_name='FFN', n_layer=3, fno_n_modes_height=16, fno_hidden_channels=16)
