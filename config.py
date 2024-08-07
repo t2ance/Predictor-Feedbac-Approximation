6b27144f4dfc4424a1736b098fa1315f6a19f6d9
t2ance <12111327@mail.sustech.edu.cn>
2024-08-07 00:05:33 -0700

    updated

--- config.py
+++ config.py
@@ -345,7 +345,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory',
                                        delay=TimeVaryingDelay(), duration=12, dt=0.02, n_dataset=250,
                                        n_sample_per_dataset=-1, ic_lower_bound=-0.5, ic_upper_bound=0.5)
-        model_config = ModelConfig(model_name='FFN', n_layer=5, fno_n_modes_height=32, fno_hidden_channels=32)
+        model_config = ModelConfig(model_name='FFN', n_layer=8, fno_n_modes_height=32, fno_hidden_channels=32)
         train_config = TrainConfig(learning_rate=1e-4, training_ratio=0.8, n_epoch=750, batch_size=128,
                                    weight_decay=1e-3, log_step=-1, lr_scheduler_type='exponential')
     else:
