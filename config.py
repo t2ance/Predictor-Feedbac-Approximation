aef155baa4a4ff5e01deefce2e6bc5eb468e4d57
t2ance <12111327@mail.sustech.edu.cn>
2024-08-08 08:12:05 -0700

    updated

--- config.py
+++ config.py
@@ -304,22 +304,22 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         if model_name == 'GRU':
             dataset_config.n_dataset = 1000
             train_config.n_epoch = 500
-            model_config.fno_n_layer = 5
-            model_config.fno_n_modes_height = 16
-            model_config.fno_hidden_channels = 16
+            model_config.gru_n_layer = 4
+            model_config.gru_layer_width = 8
         elif model_name == 'FNO':
             dataset_config.n_dataset = 1000
             train_config.n_epoch = 500
-            model_config.gru_n_layer = 10
-            model_config.gru_layer_width = 16
+            model_config.fno_n_layer = 4
+            model_config.fno_n_modes_height = 16
+            model_config.fno_hidden_channels = 32
         elif model_name == 'FNO-GRU':
             dataset_config.n_dataset = 1000
             train_config.n_epoch = 300
-            model_config.fno_gru_fno_n_layer = 5
-            model_config.fno_gru_fno_n_modes_height = 32
+            model_config.fno_gru_fno_n_layer = 4
+            model_config.fno_gru_fno_n_modes_height = 16
             model_config.fno_gru_fno_hidden_channels = 32
-            model_config.fno_gru_gru_n_layer = 2
-            model_config.fno_gru_gru_layer_width = 16
+            model_config.fno_gru_gru_n_layer = 4
+            model_config.fno_gru_gru_layer_width = 8
     elif system_ == 's2':
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory',
                                        delay=ConstantDelay(1), duration=8, dt=0.05, n_dataset=100,
@@ -355,7 +355,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
                                        n_sample_per_dataset=-1, baxter_dof=2, ic_lower_bound=0, ic_upper_bound=1,
                                        random_test_lower_bound=0, random_test_upper_bound=1)
         model_config = ModelConfig(model_name='FNO')
-        train_config = TrainConfig(learning_rate=3e-4, training_ratio=0.8, n_epoch=750, batch_size=128,
+        train_config = TrainConfig(learning_rate=3e-4, training_ratio=0.8, n_epoch=750, batch_size=64,
                                    weight_decay=1e-3, log_step=-1, lr_scheduler_type='exponential', uq_alpha=0.01,
                                    scheduled_sampling_warm_start=0, scheduled_sampling_type='linear',
                                    scheduled_sampling_k=1e-2, do_testing=True)
@@ -393,7 +393,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
                                        delay=TimeVaryingDelay(), duration=8, dt=0.02, n_dataset=250,
                                        n_sample_per_dataset=-1, ic_lower_bound=-0.5, ic_upper_bound=0.5)
         model_config = ModelConfig(model_name='FFN')
-        train_config = TrainConfig(learning_rate=1e-4, training_ratio=0.8, n_epoch=750, batch_size=128,
+        train_config = TrainConfig(learning_rate=1e-4, training_ratio=0.8, n_epoch=750, batch_size=64,
                                    weight_decay=1e-3, log_step=-1, lr_scheduler_type='exponential')
         if model_name == 'GRU':
             dataset_config.n_dataset = 500
