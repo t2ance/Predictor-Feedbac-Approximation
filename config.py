04288dd39f699cffdd385e4ce95ab8b3dba4c2f1
t2ance <12111327@mail.sustech.edu.cn>
2024-08-07 21:18:05 -0700

    updated

--- config.py
+++ config.py
@@ -397,19 +397,19 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         train_config = TrainConfig(learning_rate=1e-4, training_ratio=0.8, n_epoch=750, batch_size=32,
                                    weight_decay=1e-3, log_step=-1, lr_scheduler_type='exponential')
         if model_name == 'GRU':
-            dataset_config.n_dataset = 200
-            train_config.n_epoch = 500
+            dataset_config.n_dataset = 500
+            train_config.n_epoch = 750
             model_config.fno_n_layer = 5
             model_config.fno_n_modes_height = 16
             model_config.fno_hidden_channels = 16
         elif model_name == 'FNO':
-            dataset_config.n_dataset = 200
-            train_config.n_epoch = 500
+            dataset_config.n_dataset = 500
+            train_config.n_epoch = 750
             model_config.gru_n_layer = 10
             model_config.gru_layer_width = 16
         elif model_name == 'FNO-GRU':
-            dataset_config.n_dataset = 200
-            train_config.n_epoch = 300
+            dataset_config.n_dataset = 500
+            train_config.n_epoch = 750
             model_config.fno_gru_fno_n_layer = 5
             model_config.fno_gru_fno_n_modes_height = 32
             model_config.fno_gru_fno_hidden_channels = 32
