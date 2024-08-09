c66a57c87dce240edc0045a6e45ef2e82cebd196
t2ance <12111327@mail.sustech.edu.cn>
2024-08-08 18:48:45 -0700

    updated

--- config.py
+++ config.py
@@ -401,7 +401,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.n_epoch = 750
             model_config.gru_n_layer = 8
             model_config.gru_layer_width = 64
-            train_config.batch_size = 32
+            train_config.batch_size = 16
             train_config.learning_rate = 5e-5
         elif model_name == 'FNO':
             dataset_config.n_dataset = 1000
