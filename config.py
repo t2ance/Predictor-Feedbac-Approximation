79ccab48f14d4f2b49621d31450b5e708b813872
t2ance <12111327@mail.sustech.edu.cn>
2024-08-09 00:49:55 -0700

    updated

--- config.py
+++ config.py
@@ -414,7 +414,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             model_config.fno_hidden_channels = 32
             train_config.weight_decay = 1e-2
         elif model_name == 'FNO-GRU':
-            dataset_config.n_dataset = 300
+            dataset_config.n_dataset = 100
             train_config.n_epoch = 1000
             model_config.fno_gru_fno_n_layer = 5
             model_config.fno_gru_fno_n_modes_height = 32
