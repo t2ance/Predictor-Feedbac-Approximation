c205ae74313770eb6482a85dc024bb989a56a565
t2ance <12111327@mail.sustech.edu.cn>
2024-08-12 10:06:43 +0800

    updated

--- config.py
+++ config.py
@@ -306,7 +306,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             # train_config.n_epoch = 500
             # model_config.gru_n_layer = 5
             # model_config.gru_layer_width = 16
-            dataset_config.n_dataset = 100
+            dataset_config.n_dataset = 500
             train_config.n_epoch = 500
             model_config.gru_n_layer = 5
             model_config.gru_layer_width = 32
@@ -318,8 +318,8 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             # model_config.fno_hidden_channels = 32
             dataset_config.n_dataset = 250
             train_config.n_epoch = 500
-            model_config.fno_n_layer = 3
-            model_config.fno_n_modes_height = 8
+            model_config.fno_n_layer = 4
+            model_config.fno_n_modes_height = 16
             model_config.fno_hidden_channels = 16
         elif model_name == 'FNO-GRU':
             # dataset_config.n_dataset = 250
@@ -330,7 +330,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             # model_config.fno_gru_fno_hidden_channels = 64
             # model_config.fno_gru_gru_n_layer = 4
             # model_config.fno_gru_gru_layer_width = 32
-            dataset_config.n_dataset = 100
+            dataset_config.n_dataset = 1000
             train_config.n_epoch = 500
             train_config.weight_decay = 1e-4
             model_config.fno_gru_fno_n_layer = 2
