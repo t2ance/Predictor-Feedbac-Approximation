156148daddf29230a46db78f762200a36665b490
t2ance <12111327@mail.sustech.edu.cn>
2024-08-14 22:32:40 +0800

    updated

--- config.py
+++ config.py
@@ -322,9 +322,9 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             model_config.fno_n_layer = 3
             model_config.fno_n_modes_height = 32
             model_config.fno_hidden_channels = 32
-            model_config.fno_gru_gru_n_layer = 5
+            model_config.fno_gru_gru_n_layer = 2
             # model_config.fno_gru_gru_layer_width = 256
-            model_config.fno_gru_gru_layer_width = 16
+            model_config.fno_gru_gru_layer_width = 32
     elif system_ == 's2':
         dataset_config = DatasetConfig(recreate_training_dataset=True, data_generation_strategy='trajectory',
                                        delay=ConstantDelay(1), duration=8, dt=0.05, n_dataset=100,
@@ -371,10 +371,10 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             model_config.gru_layer_width = 64
             model_config.batch_size = 128
         elif model_name == 'FNO':
-            dataset_config.n_dataset = 300
+            dataset_config.n_dataset = 200
             train_config.n_epoch = 250
-            train_config.learning_rate = 1e-3
-            train_config.batch_size = 1024
+            train_config.learning_rate = 1e-4
+            train_config.batch_size = 256
             model_config.fno_n_layer = 5
             model_config.fno_n_modes_height = 64
             model_config.fno_hidden_channels = 64
@@ -414,7 +414,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         elif model_name == 'FNO':
             dataset_config.n_dataset = 500
             train_config.n_epoch = 250
-            train_config.batch_size = 1024
+            train_config.batch_size = 256
             model_config.fno_n_layer = 5
             model_config.fno_n_modes_height = 64
             model_config.fno_hidden_channels = 64
