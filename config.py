364974bbf63d8cf3932d109dee2ff8648520cc6d
t2ance <12111327@mail.sustech.edu.cn>
2024-08-15 10:56:53 +0800

    updated

--- config.py
+++ config.py
@@ -425,7 +425,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.learning_rate = 1e-4
             train_config.n_epoch = 250
             train_config.weight_decay = 0
-            train_config.batch_size = 512
+            train_config.batch_size = 64
             model_config.fno_n_layer = 3
             model_config.fno_n_modes_height = 64
             model_config.fno_hidden_channels = 64
