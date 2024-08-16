ad5d5a33fad5fa19a98e0f7ec635b046d60aeaa6
t2ance <12111327@mail.sustech.edu.cn>
2024-08-16 18:42:45 +0800

    updated

--- config.py
+++ config.py
@@ -354,8 +354,8 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         if model_name == 'GRU':
             dataset_config.n_training_dataset = 100
             dataset_config.n_validation_dataset = 10
-            train_config.scheduler_min_lr = 1e-4
-            train_config.n_epoch = 250
+            train_config.scheduler_min_lr = 3e-4
+            train_config.n_epoch = 500
             model_config.gru_n_layer = 5
             model_config.gru_layer_width = 16
             train_config.weight_decay = 0
@@ -364,8 +364,8 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             dataset_config.n_validation_dataset = 10
             train_config.n_epoch = 500
             model_config.fno_n_layer = 3
-            model_config.fno_n_modes_height = 32
-            model_config.fno_hidden_channels = 32
+            model_config.fno_n_modes_height = 16
+            model_config.fno_hidden_channels = 16
         elif model_name == 'FNO-GRU':
             train_config.scheduler_min_lr = 1e-4
             dataset_config.n_training_dataset = 100
