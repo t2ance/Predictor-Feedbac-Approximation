155c446fa8e20a58becec769c55d6c3dc70db036
t2ance <12111327@mail.sustech.edu.cn>
2024-08-19 21:01:32 +0800

    updated

--- config.py
+++ config.py
@@ -598,7 +598,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.n_epoch = 250
             model_config.gru_n_layer = 3
             # model_config.gru_layer_width = 32
-            model_config.gru_layer_width = 8
+            model_config.gru_layer_width = 4
             train_config.batch_size = 32
             train_config.learning_rate = 5e-5
             train_config.scheduler_min_lr = 5e-6
@@ -609,8 +609,8 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.batch_size = 512
             train_config.scheduler_min_lr = 1e-5
             model_config.fno_n_layer = 5
-            model_config.fno_n_modes_height = 32
-            model_config.fno_hidden_channels = 32
+            model_config.fno_n_modes_height = 16
+            model_config.fno_hidden_channels = 16
             train_config.weight_decay = 0
         elif model_name == 'FNO-GRU':
             dataset_config.n_training_dataset = 250
--- experiment.py
+++ experiment.py
@@ -198,59 +198,66 @@ def plot_uq_ablation(test_point, plot_name, dataset_config, train_config, model_
     plt.savefig(f"./misc/plots/{plot_name}.pdf")
 
 
-def plot_rnn_ablation(test_point, plot_name, dataset_config, train_config, model, n_row):
+def plot_rnn_ablation(test_point, plot_name, dataset_config, train_config, n_row):
     ts = dataset_config.ts
     delay = dataset_config.delay
     n_state = dataset_config.n_state
     n_point_delay = dataset_config.n_point_delay
-    fig = plt.figure(figsize=set_size(width=fig_width, fraction=1.4, subplots=(n_row, 4), height_add=0.6))
-    subfigs = fig.subfigures(nrows=1, ncols=4)
-    no_fig, rnn_fig, gru_fig, lstm_fig = subfigs
+    fig = plt.figure(figsize=set_size(width=fig_width, fraction=1.4, subplots=(n_row, 3), height_add=0.6))
+    subfigs = fig.subfigures(nrows=1, ncols=3)
+    no_fig, gru_fig, lstm_fig = subfigs
     no_fig.suptitle('FNO')
-    rnn_fig.suptitle('FNO-RNN')
     gru_fig.suptitle('FNO-GRU')
     lstm_fig.suptitle('FNO-LSTM')
-    numerical_axes = rnn_fig.subplots(nrows=n_row, ncols=1, gridspec_kw={'hspace': 0.5})
     no_axes = no_fig.subplots(nrows=n_row, ncols=1, gridspec_kw={'hspace': 0.5})
-    switching_axes = gru_fig.subplots(nrows=n_row, ncols=1, gridspec_kw={'hspace': 0.5})
+    gru_axes = gru_fig.subplots(nrows=n_row, ncols=1, gridspec_kw={'hspace': 0.5})
+    lstm_axes = lstm_fig.subplots(nrows=n_row, ncols=1, gridspec_kw={'hspace': 0.5})
 
     print(f'Begin simulation {plot_name}')
-    numerical = simulation(dataset_config=dataset_config, train_config=train_config, model=model, Z0=test_point,
-                           method='numerical')
+
+    dataset_config, model_config, train_config = config.get_config(system_='s5', model_name='FNO')
+    model, model_loaded = load_model(train_config, model_config, dataset_config)
+    model_config.load_model(run, model)
     no = simulation(dataset_config=dataset_config, train_config=train_config, model=model, Z0=test_point, method='no')
+
+    dataset_config, model_config, train_config = config.get_config(system_='s5', model_name='FNO-GRU')
+    model, model_loaded = load_model(train_config, model_config, dataset_config)
+    model_config.load_model(run, model)
+    no = simulation(dataset_config=dataset_config, train_config=train_config, model=model, Z0=test_point, method='no')
+
     switching = simulation(dataset_config=dataset_config, train_config=train_config, model=model, Z0=test_point,
                            method='switching')
     print(f'End simulation {plot_name}')
 
-    min_p, max_p = interval(min(numerical.P_numerical.min(), no.P_no.min(), switching.P_switching.min()),
-                            max(numerical.P_numerical.max(), no.P_no.max(), switching.P_switching.max()))
+    min_p, max_p = interval(min(no.P_numerical.min(), no.P_no.min(), switching.P_switching.min()),
+                            max(no.P_numerical.max(), no.P_no.max(), switching.P_switching.max()))
 
-    plot_comparison(ts, [numerical.P_numerical], numerical.Z, delay, n_point_delay, None, n_state, ylim=[min_p, max_p],
-                    ax=numerical_axes[0], comment=False)
+    plot_comparison(ts, [no.P_numerical], no.Z, delay, n_point_delay, None, n_state, ylim=[min_p, max_p],
+                    ax=lstm_axes[0], comment=False)
     plot_comparison(ts, [no.P_no], no.Z, delay, n_point_delay, None, n_state, ylim=[min_p, max_p], ax=no_axes[0],
                     comment=False)
     plot_comparison(ts, [switching.P_switching], switching.Z, delay, n_point_delay, None, n_state, ylim=[min_p, max_p],
-                    ax=switching_axes[0], comment=True)
-    min_d, max_d = interval(min(numerical.D_numerical.min(), no.D_no.min(), switching.D_switching.min()),
-                            max(numerical.D_numerical.max(), no.D_no.max(), switching.D_switching.max()))
+                    ax=gru_axes[0], comment=True)
+    min_d, max_d = interval(min(no.D_numerical.min(), no.D_no.min(), switching.D_switching.min()),
+                            max(no.D_numerical.max(), no.D_no.max(), switching.D_switching.max()))
 
-    plot_difference(ts, [numerical.P_numerical], numerical.Z, delay, n_point_delay, None, n_state, ylim=[min_d, max_d],
-                    ax=numerical_axes[1], comment=False, differences=[numerical.D_numerical])
+    plot_difference(ts, [no.P_numerical], no.Z, delay, n_point_delay, None, n_state, ylim=[min_d, max_d],
+                    ax=lstm_axes[1], comment=False, differences=[no.D_numerical])
     plot_difference(ts, [no.P_no], no.Z, delay, n_point_delay, None, n_state, ylim=[min_d, max_d], ax=no_axes[1],
                     comment=False, differences=[no.D_no])
     plot_difference(ts, [switching.P_switching], switching.Z, delay, n_point_delay, None, n_state, ylim=[min_d, max_d],
-                    ax=switching_axes[1], comment=True, differences=[switching.D_switching])
+                    ax=gru_axes[1], comment=True, differences=[switching.D_switching])
 
-    min_u, max_u = interval(min(numerical.U.min(), no.U.min(), switching.U.min()),
-                            max(numerical.U.max(), no.U.max(), switching.U.max()))
-    plot_control(ts, numerical.U, None, n_point_delay, ax=numerical_axes[2], comment=False, ylim=[min_u, max_u],
+    min_u, max_u = interval(min(no.U.min(), no.U.min(), switching.U.min()),
+                            max(no.U.max(), no.U.max(), switching.U.max()))
+    plot_control(ts, no.U, None, n_point_delay, ax=lstm_axes[2], comment=False, ylim=[min_u, max_u],
                  linestyle='--')
     plot_control(ts, no.U, None, n_point_delay, ax=no_axes[2], comment=False, ylim=[min_u, max_u])
-    plot_switch_segments(ts, switching, n_point_delay(0), ax=switching_axes[2], comment=True, ylim=[min_u, max_u])
+    plot_switch_segments(ts, switching, n_point_delay(0), ax=gru_axes[2], comment=True, ylim=[min_u, max_u])
 
     if n_row == 4:
         q_des = np.array([dataset_config.system.q_des(t) for t in ts])
-        q_numerical = q_des - numerical.Z[:, :2]
+        q_numerical = q_des - no.Z[:, :2]
         q_no = q_des - no.Z[:, :2]
         q_switching = q_des - switching.Z[:, :2]
         n_point_start = n_point_delay(0)
@@ -258,10 +265,10 @@ def plot_rnn_ablation(test_point, plot_name, dataset_config, train_config, model
         q_numerical = q_numerical[n_point_start:]
         q_no = q_no[n_point_start:]
         q_switching = q_switching[n_point_start:]
-        plot_q(ts[n_point_start:], [q_numerical], q_des, None, dataset_config.system.n_input, ax=numerical_axes[3],
+        plot_q(ts[n_point_start:], [q_numerical], q_des, None, dataset_config.system.n_input, ax=lstm_axes[3],
                comment=False)
         plot_q(ts[n_point_start:], [q_no], q_des, None, dataset_config.system.n_input, ax=no_axes[3], comment=False)
-        plot_q(ts[n_point_start:], [q_switching], q_des, None, dataset_config.system.n_input, ax=switching_axes[3],
+        plot_q(ts[n_point_start:], [q_switching], q_des, None, dataset_config.system.n_input, ax=gru_axes[3],
                comment=True)
 
     plt.savefig(f"./misc/plots/{plot_name}.pdf")
