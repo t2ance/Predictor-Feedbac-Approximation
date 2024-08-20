5a72d1de5c534b622b99cf0ddf1c4be9e03efe8d
t2ance <12111327@mail.sustech.edu.cn>
2024-08-20 11:47:06 +0800

    updated

--- config.py
+++ config.py
@@ -607,10 +607,10 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         elif model_name == 'FNO':
             dataset_config.n_training_dataset = 250
             dataset_config.n_validation_dataset = 10
-            train_config.n_epoch = 250
+            train_config.n_epoch = 500
             train_config.batch_size = 512
             train_config.scheduler_min_lr = 1e-5
-            model_config.fno_n_layer = 3
+            model_config.fno_n_layer = 4
             model_config.fno_n_modes_height = 8
             model_config.fno_hidden_channels = 8
             train_config.weight_decay = 0
