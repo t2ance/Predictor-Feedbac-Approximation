1363b34b943d4f55c3379d2ab6485cacdfb8d5d1
t2ance <12111327@mail.sustech.edu.cn>
2024-08-21 20:40:02 +0800

    updated

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
-from neuralop.models import FNO1d
+# from neuralop.models import FNO1d
+from neuralop.models import TFNO1d
 from torch import nn
 
 
@@ -203,10 +204,8 @@ class FNOProjection(torch.nn.Module):
         self.n_state = n_state
         self.mse_loss = torch.nn.MSELoss()
         self.n_modes_height = n_modes_height
-        self.fno = FNO1d(n_modes_height=n_modes_height, n_layers=n_layers, hidden_channels=hidden_channels,
-                         in_channels=2,
-                         # in_channels=n_state + 1,
-                         out_channels=1)
+        self.fno = TFNO1d(n_modes_height=n_modes_height, n_layers=n_layers, hidden_channels=hidden_channels,
+                          in_channels=2, out_channels=1)
         self.projection = torch.nn.Linear(in_features=n_modes_height * 2, out_features=n_state)
         self.linear_decoder = LinearDecoder(n_state, 1, n_modes_height)
         self.linear_functional = LinearFunctional(1, n_state, n_modes_height)
@@ -310,54 +309,6 @@ class LinearFunctional(nn.Module):
 
         # Integrate the conjugate product in physical space by summing Fourier coefficients
         return 2 * torch.sum(x, dim=-1) - x[..., 0]
-
-
-class FNOTwoStage(torch.nn.Module):
-    def __init__(self, n_modes_height: int, hidden_channels: int, n_state: int, n_layers: int, dt: float, *args,
-                 **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fno = FNO1d(n_modes_height=n_modes_height, n_layers=n_layers, hidden_channels=hidden_channels,
-                         in_channels=n_state + 1, out_channels=n_state)
-        self.integral_net_p = FNO1d(n_modes_height=n_modes_height, n_layers=n_layers, hidden_channels=hidden_channels,
-                                    in_channels=n_state, out_channels=n_state)
-        self.integral_net_u = FNO1d(n_modes_height=n_modes_height, n_layers=n_layers, hidden_channels=hidden_channels,
-                                    in_channels=1, out_channels=n_state)
-        self.integral_net_p_u = FNO1d(n_modes_height=n_modes_height, n_layers=n_layers, hidden_channels=hidden_channels,
-                                      in_channels=n_state, out_channels=n_state)
-        self.dt = dt
-
-    def forward(self, z_u: torch.Tensor, label: torch.Tensor = None):
-        U = z_u[:, 2:]
-        Z = z_u[:, :2]
-        # out = self.fno(Z_U.unsqueeze(-2)).squeeze(1)
-
-        P = self.fno(torch.einsum('bi,bj->bij', torch.cat([Z, torch.ones_like(Z)[:, 0:1]], dim=1), U))
-        # find p
-        # P = torch.zeros((Z_U.shape[0], Z.shape[1], U.shape[1]))
-        # P[:, :, 0] = Z
-        # for j in range(U.shape[1] - 1):
-        #     Z2_t = P[:, 1, j]
-        #     Z1_t_dot = Z2_t - Z2_t ** 2 * U[:, j]
-        #     Z2_t_dot = U[:, j]
-        #     partial = torch.hstack([Z1_t_dot.reshape(-1, 1), Z2_t_dot.reshape(-1, 1)]) * self.dt
-        #     P[:, :, j + 1] = P[:, :, j] + partial
-
-        # predict_integral_general
-        # integral = torch.zeros_like(Z_U[:, :2])
-        # for i in range(P.shape[-1]):
-        #     Z2_t = P[:, 1, i]
-        #     U_delay = U[:, i]
-        #     Z1_t_dot = Z2_t - Z2_t ** 2 * U_delay
-        #     Z2_t_dot = U_delay
-        #     partial = torch.hstack([Z1_t_dot.reshape(-1, 1), Z2_t_dot.reshape(-1, 1)]) * self.dt
-        #     integral += partial
-        p_ = self.integral_net_p(P)
-        u_ = self.integral_net_u(U.unsqueeze(1))
-        p_u = self.integral_net_p_u(p_ + u_)
-        x = p_u.sum(axis=-1) * self.dt + Z
-        if label is None:
-            return x
-        return x, self.mse_loss(x, label)
 
 
 if __name__ == '__main__':
