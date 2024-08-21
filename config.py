1a79f1b998f624733530828c18ac34ee09e8c3dc
t2ance <12111327@mail.sustech.edu.cn>
2024-08-21 20:47:01 +0800

    updated

--- config.py
+++ config.py
@@ -343,7 +343,7 @@ class DatasetConfig:
 
 def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=None):
     if system_ == 's1':
-        dataset_config = DatasetConfig(recreate_dataset=False, data_generation_strategy='trajectory',
+        dataset_config = DatasetConfig(recreate_dataset=True, data_generation_strategy='trajectory',
                                        delay=ConstantDelay(1), duration=6, dt=0.1, n_training_dataset=900,
                                        n_validation_dataset=100, n_sample_per_dataset=-1, ic_lower_bound=0,
                                        ic_upper_bound=1, random_test_lower_bound=0, random_test_upper_bound=1)
