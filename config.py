62e4340443e73057219ebe141af07ef753f40016
t2ance <12111327@mail.sustech.edu.cn>
2024-08-26 21:59:49 +0800

    updated

--- config.py
+++ config.py
@@ -656,7 +656,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         elif model_name == 'FNO-GRU':
             dataset_config.n_training_dataset = 200
             dataset_config.n_validation_dataset = 10
-            train_config.learning_rate = 1e-5
+            train_config.learning_rate = 1e-6
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 300
