5af7a26dd7f4221d95f57c77885c0edbe37f191f
t2ance <12111327@mail.sustech.edu.cn>
2024-08-28 23:01:25 +0800

    updated

--- config.py
+++ config.py
@@ -573,16 +573,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
                                    weight_decay=1e-3, log_step=-1, lr_scheduler_type='exponential', uq_alpha=0.01,
                                    scheduled_sampling_warm_start=0, scheduled_sampling_type='linear',
                                    scheduled_sampling_k=1e-2, scheduler_min_lr=1e-5)
-        if model_name == 'GRU':
-            dataset_config.n_training_dataset = 200
-            dataset_config.n_validation_dataset = 20
-            train_config.n_epoch = 500
-            model_config.gru_n_layer = 5
-            model_config.gru_layer_width = 16
-            model_config.batch_size = 512
-            train_config.learning_rate = 1e-5
-            train_config.scheduler_min_lr = 1e-5
-        elif model_name == 'FNO':
+        if model_name == 'FNO':
             dataset_config.n_training_dataset = 200
             dataset_config.n_validation_dataset = 20
             train_config.n_epoch = 800
@@ -593,6 +584,15 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             model_config.fno_n_modes_height = 32
             model_config.fno_hidden_channels = 32
             train_config.weight_decay = 1e-1
+        elif model_name == 'GRU':
+            dataset_config.n_training_dataset = 200
+            dataset_config.n_validation_dataset = 20
+            train_config.n_epoch = 500
+            model_config.gru_n_layer = 5
+            model_config.gru_layer_width = 16
+            model_config.batch_size = 512
+            train_config.learning_rate = 1e-5
+            train_config.scheduler_min_lr = 1e-5
         elif model_name == 'LSTM':
             dataset_config.n_training_dataset = 200
             dataset_config.n_validation_dataset = 20
@@ -614,8 +614,8 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             model_config.fno_n_layer = 6
             model_config.fno_n_modes_height = 32
             model_config.fno_hidden_channels = 32
-            model_config.gru_n_layer = 4
-            model_config.gru_layer_width = 64
+            model_config.gru_n_layer = 5
+            model_config.gru_layer_width = 16
         elif model_name == 'FNO-LSTM':
             dataset_config.n_training_dataset = 200
             dataset_config.n_validation_dataset = 20
