e39dc259605609396a2c77467f9d1ca51731d9eb
t2ance <12111327@mail.sustech.edu.cn>
2024-09-01 18:32:44 +0800

    updated

--- config.py
+++ config.py
@@ -754,8 +754,8 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
         elif model_name == 'DeepONet-LSTM':
             dataset_config.n_training_dataset = 500
             dataset_config.n_validation_dataset = 10
-            train_config.learning_rate = 2e-6
-            train_config.scheduler_min_lr = 2e-6
+            train_config.learning_rate = 1e-6
+            train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 600
             train_config.weight_decay = 1
