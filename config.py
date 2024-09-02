f1cd2ff437b6463b17affd611e292156aeb42bbf
t2ance <12111327@mail.sustech.edu.cn>
2024-09-02 18:01:13 +0800

    updated

--- config.py
+++ config.py
@@ -592,10 +592,10 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
 
-            model_config.deeponet_hidden_size = 128
+            model_config.deeponet_hidden_size = 16
             model_config.deeponet_n_layer = 5
         elif model_name == 'GRU':
-            train_config.n_epoch = 100
+            train_config.n_epoch = 50
             model_config.gru_n_layer = 3
             model_config.gru_layer_width = 4
             model_config.batch_size = 512
