f3c8642ee52cf353935e1a852fcd2906d2406da2
t2ance <12111327@mail.sustech.edu.cn>
2024-09-02 21:02:02 +0800

    updated

--- config.py
+++ config.py
@@ -714,7 +714,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 100
-            train_config.weight_decay = 1e-3
+            # train_config.weight_decay = 1e-3
             model_config.fno_n_layer = 4
             model_config.fno_n_modes_height = 16
             model_config.fno_hidden_channels = 16
@@ -728,7 +728,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 100
-            train_config.weight_decay = 1e-3
+            # train_config.weight_decay = 1e-3
             model_config.fno_n_layer = 4
             model_config.fno_n_modes_height = 16
             model_config.fno_hidden_channels = 16
@@ -742,7 +742,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 100
-            train_config.weight_decay = 1e-3
+            # train_config.weight_decay = 1e-3
 
             model_config.gru_n_layer = 5
             model_config.gru_layer_width = 16
@@ -756,7 +756,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 100
-            train_config.weight_decay = 1e-3
+            # train_config.weight_decay = 1e-3
 
             model_config.lstm_n_layer = 3
             model_config.lstm_layer_width = 16
