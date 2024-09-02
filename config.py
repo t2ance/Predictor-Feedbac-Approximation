13f8c1c236dfee9c9f4cc7d8293e16d6517e0f6f
t2ance <12111327@mail.sustech.edu.cn>
2024-09-02 16:39:44 +0800

    updated

--- config.py
+++ config.py
@@ -579,20 +579,20 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
                                    scheduled_sampling_warm_start=0, scheduled_sampling_type='linear',
                                    scheduled_sampling_k=1e-2, scheduler_min_lr=1e-5)
         if model_name == 'FNO':
-            train_config.n_epoch = 1000
+            train_config.n_epoch = 100
             train_config.learning_rate = 1e-4
             train_config.scheduler_min_lr = 1e-5
             train_config.batch_size = 512
             model_config.fno_n_layer = 5
             model_config.fno_n_modes_height = 16
             model_config.fno_hidden_channels = 16
-            train_config.weight_decay = 0.01
+            # train_config.weight_decay = 0.01
         elif model_name == 'DeepONet':
+            train_config.n_epoch = 100
             train_config.learning_rate = 1e-5
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
-            train_config.n_epoch = 300
-            train_config.weight_decay = 0.01
+            # train_config.weight_decay = 0.01
 
             model_config.deeponet_hidden_size = 64
             model_config.deeponet_n_layer = 3
@@ -604,8 +604,8 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.learning_rate = 1e-6
             train_config.scheduler_min_lr = 1e-6
         elif model_name == 'LSTM':
-            train_config.weight_decay = 1
             train_config.n_epoch = 100
+            # train_config.weight_decay = 1
             train_config.learning_rate = 1e-6
             train_config.scheduler_min_lr = 1e-6
             model_config.lstm_n_layer = 3
