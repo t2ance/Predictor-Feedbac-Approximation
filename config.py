bc6dface5db41136e585cafaf2c2441a969b72e3
t2ance <12111327@mail.sustech.edu.cn>
2024-09-04 10:16:51 +0800

    updated

--- config.py
+++ config.py
@@ -595,10 +595,10 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             model_config.fno_hidden_channels = 64
         elif model_name == 'DeepONet':
             train_config.n_epoch = 100
-            train_config.learning_rate = 5e-5
+            train_config.learning_rate = 1e-5
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
-            model_config.deeponet_hidden_size = 256
+            model_config.deeponet_hidden_size = 128
             model_config.deeponet_n_layer = 5
         elif model_name == 'GRU':
             train_config.n_epoch = 50
