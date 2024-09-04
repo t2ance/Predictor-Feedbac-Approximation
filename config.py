a51919d598ebe31a784b936c6b54ab65dbc8ea8a
t2ance <12111327@mail.sustech.edu.cn>
2024-09-04 19:42:01 +0800

    updated

--- config.py
+++ config.py
@@ -596,7 +596,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             model_config.fno_n_layer = 5
-            model_config.fno_n_modes_height = 32
+            model_config.fno_n_modes_height = 64
             model_config.fno_hidden_channels = 64
         elif model_name == 'DeepONet':
             train_config.n_epoch = 100
