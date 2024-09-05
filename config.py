600dcb08a8b1977fb44d86c4882236331b14eed0
t2ance <12111327@mail.sustech.edu.cn>
2024-09-05 18:57:43 +0800

    updated

--- config.py
+++ config.py
@@ -490,7 +490,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.n_epoch = 100
             train_config.weight_decay = 0
 
-            model_config.gru_n_layer = 6
+            model_config.gru_n_layer = 3
             model_config.gru_layer_width = 4
 
             model_config.fno_n_layer = 5
@@ -509,7 +509,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.n_epoch = 100
             train_config.weight_decay = 0
 
-            model_config.lstm_n_layer = 6
+            model_config.lstm_n_layer = 3
             model_config.lstm_layer_width = 4
 
             model_config.fno_n_layer = 5
@@ -528,7 +528,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.n_epoch = 100
             train_config.weight_decay = 0
 
-            model_config.gru_n_layer = 6
+            model_config.gru_n_layer = 3
             model_config.gru_layer_width = 4
 
             model_config.deeponet_hidden_size = 64
@@ -546,7 +546,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.n_epoch = 100
             train_config.weight_decay = 0
 
-            model_config.lstm_n_layer = 6
+            model_config.lstm_n_layer = 3
             model_config.lstm_layer_width = 4
 
             model_config.deeponet_hidden_size = 64
