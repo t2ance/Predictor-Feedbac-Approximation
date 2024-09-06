8954247c24d9bbfa61dcb426ac6f8a5a0a8ca206
t2ance <12111327@mail.sustech.edu.cn>
2024-09-06 19:45:36 +0800

    updated

--- config.py
+++ config.py
@@ -533,7 +533,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
 
             train_config.batch_size = 512
             train_config.n_epoch = 100
-            train_config.weight_decay = 1e-2
+            train_config.weight_decay = 1e-1
 
             model_config.gru_n_layer = 3
             model_config.gru_layer_width = 8
@@ -552,7 +552,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
 
             train_config.batch_size = 512
             train_config.n_epoch = 100
-            train_config.weight_decay = 1e-2
+            train_config.weight_decay = 1e-1
 
             model_config.lstm_n_layer = 3
             model_config.lstm_layer_width = 8
