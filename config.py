0ee904182fd2107c52e707724ae2d2bf43f5eaec
t2ance <12111327@mail.sustech.edu.cn>
2024-09-07 10:42:28 +0800

    updated

--- config.py
+++ config.py
@@ -535,7 +535,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.weight_decay = 3e-3
 
             model_config.gru_n_layer = 3
-            model_config.gru_layer_width = 8
+            model_config.gru_layer_width = 16
 
             model_config.deeponet_hidden_size = 64
             model_config.deeponet_n_layer = 3
@@ -554,7 +554,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.weight_decay = 3e-3
 
             model_config.lstm_n_layer = 3
-            model_config.lstm_layer_width = 8
+            model_config.lstm_layer_width = 16
 
             model_config.deeponet_hidden_size = 64
             model_config.deeponet_n_layer = 3
