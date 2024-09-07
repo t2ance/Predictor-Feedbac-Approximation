9c6ef9e66bd8a9feb98b9fe58d2fa0e050d4d4de
t2ance <12111327@mail.sustech.edu.cn>
2024-09-07 22:58:06 +0800

    updated

---
+++ main_sweep.py
@@ -0,0 +1,159 @@
+import wandb
+
+from config import get_config
+from main import main
+from utils import print_args, set_everything
+
+
+def set_config(config, dataset_config, model_config, train_config):
+    train_config.two_stage = False
+    train_config.train_first_stage = False
+    train_config.residual = True
+    train_config.zero_init = True
+    train_config.auxiliary_loss = False
+    train_config.lr_scheduler_type = 'cosine_annealing_with_warmup'
+    train_config.scheduler_min_lr = 0
+    train_config.batch_size = 2048
+    train_config.n_epoch = 20
+
+    train_config.learning_rate = config.learning_rate
+    train_config.weight_decay = config.weight_decay
+
+    model_config.gru_n_layer = config.gru_n_layer
+    model_config.gru_layer_width = config.gru_layer_width
+
+    model_config.deeponet_hidden_size = config.deeponet_hidden_size
+    model_config.deeponet_n_layer = config.deeponet_n_layer
+
+    model_config.lstm_n_layer = config.lstm_n_layer
+    model_config.lstm_layer_width = config.lstm_layer_width
+
+    model_config.fno_n_layer = config.fno_n_layer
+    model_config.fno_n_modes_height = config.fno_n_modes_height
+    model_config.fno_hidden_channels = config.fno_hidden_channels
+
+    print_args(dataset_config)
+    print_args(model_config)
+    print_args(train_config)
+
+
+def get_parameters(system: str, model_name: str):
+    ffn, rnn = model_name.split('-')
+    parameters = {
+        'learning_rate': {
+            'distribution': 'log_uniform',
+            'min': 1e-6,
+            'max': 1e-3
+        },
+        'weight_decay': {
+            'distribution': 'log_uniform',
+            'min': 0,
+            'max': 1e-1
+        }
+    }
+
+    if ffn == 'FNO':
+        parameters.update({
+            'fno_n_layer': {
+                'distribution': 'int_uniform',
+                'min': 1,
+                'max': 6
+            },
+            'fno_n_modes_height': {
+                'distribution': 'log_uniform',
+                'min': 4,
+                'max': 64
+            },
+            'fno_hidden_channels': {
+                'distribution': 'log_uniform',
+                'min': 4,
+                'max': 128
+            }
+        })
+    elif ffn == 'DeepONet':
+        parameters.update({
+            'deeponet_n_layer': {
+                'distribution': 'int_uniform',
+                'min': 1,
+                'max': 6
+            },
+            'deeponet_hidden_size': {
+                'distribution': 'log_uniform',
+                'min': 4,
+                'max': 64
+            },
+        })
+    else:
+        raise NotImplementedError()
+
+    if rnn == 'GRU':
+        parameters.update({
+            'gru_n_layer': {
+                'distribution': 'int_uniform',
+                'min': 1,
+                'max': 6
+            },
+            'gru_layer_width': {
+                'distribution': 'log_uniform',
+                'min': 4,
+                'max': 128
+            }
+        })
+    elif rnn == 'LSTM':
+        parameters.update({
+            'lstm_n_layer': {
+                'distribution': 'int_uniform',
+                'min': 1,
+                'max': 6
+            },
+            'lstm_layer_width': {
+                'distribution': 'log_uniform',
+                'min': 4,
+                'max': 128
+            }
+        })
+    else:
+        raise NotImplementedError()
+
+    return parameters
+
+
+def do_sweep(system, model_name):
+    wandb.login(key='ed146cfe3ec2583a2207a02edcc613f41c4e2fb1')
+    sweep_config = {
+        'method': 'bayes',
+        'metric': {
+            'name': 'l2',
+            'goal': 'minimize'
+        },
+        'parameters': get_parameters(system, model_name)
+    }
+
+    sweep_id = wandb.sweep(sweep_config, project="no")
+
+    def train(config=None):
+        with wandb.init(config=config):
+            config = wandb.config
+            dataset_config, model_config, train_config = get_config(system_=system, model_name=model_name)
+            set_config(config, dataset_config, model_config, train_config)
+            results, model = main(dataset_config, model_config, train_config)
+            wandb.log(
+                {
+                    'l2': results['no'].l2
+                }
+            )
+
+    wandb.agent(sweep_id, train)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    set_everything(0)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', type=str)
+    parser.add_argument('-model_name', type=str)
+    args = parser.parse_args()
+
+    do_sweep(system=args.s, model_name=args.model_name)
