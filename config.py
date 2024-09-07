0463ddf43a4e2a805bd96a127ce9ba0baaea2401
t2ance <12111327@mail.sustech.edu.cn>
2024-09-07 09:49:13 +0800

    updated

--- config.py
+++ config.py
@@ -604,7 +604,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.train_first_stage = False
             train_config.residual = True
             train_config.lr_scheduler_type = 'cosine_annealing_with_warmup'
-            train_config.learning_rate = 1e-6
+            train_config.learning_rate = 1e-5
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 100
@@ -620,7 +620,7 @@ def get_config(system_, n_iteration=None, duration=None, delay=None, model_name=
             train_config.train_first_stage = False
             train_config.residual = True
             train_config.lr_scheduler_type = 'cosine_annealing_with_warmup'
-            train_config.learning_rate = 1e-6
+            train_config.learning_rate = 1e-5
             train_config.scheduler_min_lr = 1e-6
             train_config.batch_size = 512
             train_config.n_epoch = 100
