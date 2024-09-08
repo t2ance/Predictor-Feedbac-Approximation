d53f31a936b363b6c939c9b6e6d93ec573d53f53
t2ance <12111327@mail.sustech.edu.cn>
2024-09-08 20:16:35 +0800

    updated

--- config.py
+++ config.py
@@ -66,8 +66,16 @@ class ModelConfig:
     def get_model(self, run, train_config, dataset_config, version: str = None):
         if version is None:
             version = self.model_version
-        model_name = self.model_name.replace('FNO', 'FNOProjection')
+        if 'FNO' in self.model_name:
+            model_name = self.model_name.replace('FNO', 'FNOProjection')
+        elif self.model_name == 'GRU':
+            model_name = 'GRUNet'
+        elif self.model_name == 'LSTM':
+            model_name = 'LSTMNet'
+        else:
+            model_name = self.model_name
         model_artifact = run.use_artifact(f"{model_name}-{self.system}:{version}")
+        model_name = self.model_name
         metadata = model_artifact.metadata
         model_dir = model_artifact.download()
         model_path = os.path.join(model_dir, "model.pth")
