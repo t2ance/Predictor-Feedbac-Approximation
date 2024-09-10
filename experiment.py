b9623e9cbdba4abea818d8968a515fd9ec81b0ca
t2ance <12111327@mail.sustech.edu.cn>
2024-09-10 11:19:07 +0800

    updated

--- experiment.py
+++ experiment.py
@@ -440,14 +440,14 @@ if __name__ == '__main__':
             dataset_config.random_test_upper_bound = 2
             train_config.uq_gamma = 0.01
             alphas = [0.02, 0.1, 0.5]
-            metric_list = ['l2_p_z', 'rl2_p_z']
+            # metric_list = ['l2_p_z', 'rl2_p_z']
         elif args.s == 's9':
             model = deeponet_gru
             dataset_config.random_test_lower_bound = 1
             dataset_config.random_test_upper_bound = 1.5
             train_config.uq_gamma = 0.01
             alphas = [0.02, 0.1, 0.5]
-            metric_list = ['l2_p_z', 'rl2_p_z']
+            # metric_list = ['l2_p_z', 'rl2_p_z']
         else:
             raise NotImplementedError()
     else:
