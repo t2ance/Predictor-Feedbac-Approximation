c6df9adfbcfcf7546989fe06307ecd972edb4a6b
t2ance <12111327@mail.sustech.edu.cn>
2024-09-11 10:32:36 +0800

    updated

--- experiment.py
+++ experiment.py
@@ -400,8 +400,8 @@ if __name__ == '__main__':
             deeponet_gru_gp = None
             deeponet_lstm_gp = None
 
-            dataset_config.random_test_lower_bound = 1
-            dataset_config.random_test_upper_bound = 1.5
+            dataset_config.random_test_lower_bound = -2
+            dataset_config.random_test_upper_bound = 2
             train_config.uq_gamma = 0.01
             train_config.uq_alpha = 0.1
         elif args.s == 's9':
@@ -432,8 +432,8 @@ if __name__ == '__main__':
             # deeponet_gru_gp = None
             deeponet_lstm_gp = None
 
-            dataset_config.random_test_lower_bound = 1
-            dataset_config.random_test_upper_bound = 1.5
+            dataset_config.random_test_lower_bound = -2
+            dataset_config.random_test_upper_bound = 2
             train_config.uq_gamma = 0.01
             train_config.uq_alpha = 0.1
         else:
