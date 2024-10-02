3cff8da2cab3ea53ba881a43643c9c264fc4bc25
t2ance <12111327@mail.sustech.edu.cn>
2024-10-02 19:43:42 +0800

    updated

--- main_sweep.py
+++ main_sweep.py
@@ -84,13 +84,13 @@ def get_parameters(system: str, model_name: str):
     parameters = {
         'learning_rate': {
             'distribution': 'log_uniform_values',
-            'min': 1e-4,
+            'min': 1e-5,
             'max': 1e-2
         },
         'weight_decay': {
             'distribution': 'log_uniform_values',
             'min': 1e-5,
-            'max': 1e-1
+            'max': 1e-2
         }
     }
 
