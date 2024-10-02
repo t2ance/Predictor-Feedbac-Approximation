b49a9e7c5cdae267cf432a268a8ffafba3d2ba94
t2ance <12111327@mail.sustech.edu.cn>
2024-10-02 22:33:01 +0800

    updated

--- utils.py
+++ utils.py
@@ -87,18 +87,20 @@ def load_model(train_config, model_config, dataset_config, n_param_out: bool = F
     seq_len = dataset_config.max_n_point_delay()
     if model_name == 'DeepONet':
         model = DeepONet(hidden_size=model_config.deeponet_hidden_size, n_layer=model_config.deeponet_n_layer,
-                         n_input=n_input, n_state=n_state, seq_len=seq_len)
+                         n_input=n_input, n_state=n_state, seq_len=seq_len, use_t=train_config.use_t)
     elif model_name == 'FNO':
         n_modes_height = model_config.fno_n_modes_height
         hidden_channels = model_config.fno_hidden_channels
         model = FNOProjection(n_modes_height=n_modes_height, hidden_channels=hidden_channels,
-                              n_layers=model_config.fno_n_layer, n_input=n_input, n_state=n_state, seq_len=seq_len)
+                              n_layers=model_config.fno_n_layer, n_input=n_input, n_state=n_state, seq_len=seq_len,
+                              use_t=train_config.use_t)
     elif model_name == 'GRU':
         model = GRUNet(hidden_size=model_config.gru_hidden_size, num_layers=model_config.gru_n_layer,
-                       output_size=n_state, n_input=n_input, n_state=n_state, seq_len=seq_len)
+                       output_size=n_state, n_input=n_input, n_state=n_state, seq_len=seq_len, use_t=train_config.use_t)
     elif model_name == 'LSTM':
         model = LSTMNet(hidden_size=model_config.lstm_hidden_size, num_layers=model_config.lstm_n_layer,
-                        output_size=n_state, n_input=n_input, n_state=n_state, seq_len=seq_len)
+                        output_size=n_state, n_input=n_input, n_state=n_state, seq_len=seq_len,
+                        use_t=train_config.use_t)
     elif model_name in ['FNO-GRU', 'Inverted-FNO-GRU']:
         model = TimeAwareNeuralOperator(
             ffn='FNO', rnn='GRU',
@@ -109,10 +111,10 @@ def load_model(train_config, model_config, dataset_config, n_param_out: bool = F
                 'fno_n_layers': model_config.fno_n_layer,
                 'gru_n_layers': model_config.gru_n_layer,
                 'gru_hidden_size': model_config.gru_hidden_size
-            }, n_input=n_input, n_state=n_state, seq_len=seq_len)
+            }, n_input=n_input, n_state=n_state, seq_len=seq_len, use_t=train_config.use_t)
     elif model_name in ['FNO-LSTM', 'Inverted-FNO-LSTM']:
         model = TimeAwareNeuralOperator(
-            ffn='FNO', rnn='LSTM', n_input=n_input, n_state=n_state, seq_len=seq_len,
+            ffn='FNO', rnn='LSTM', n_input=n_input, n_state=n_state, seq_len=seq_len, use_t=train_config.use_t,
             invert=model_name.startswith('Inverted'),
             params={
                 'fno_n_modes_height': model_config.fno_n_modes_height,
@@ -123,7 +125,7 @@ def load_model(train_config, model_config, dataset_config, n_param_out: bool = F
             })
     elif model_name in ['DeepONet-GRU', 'Inverted-DeepONet-GRU']:
         model = TimeAwareNeuralOperator(
-            ffn='DeepONet', rnn='GRU', n_input=n_input, n_state=n_state, seq_len=seq_len,
+            ffn='DeepONet', rnn='GRU', n_input=n_input, n_state=n_state, seq_len=seq_len, use_t=train_config.use_t,
             invert=model_name.startswith('Inverted'),
             params={
                 'deeponet_hidden_size': model_config.deeponet_hidden_size,
@@ -133,7 +135,7 @@ def load_model(train_config, model_config, dataset_config, n_param_out: bool = F
             })
     elif model_name in ['DeepONet-LSTM', 'Inverted-DeepONet-LSTM']:
         model = TimeAwareNeuralOperator(
-            ffn='DeepONet', rnn='LSTM', n_input=n_input, n_state=n_state, seq_len=seq_len,
+            ffn='DeepONet', rnn='LSTM', n_input=n_input, n_state=n_state, seq_len=seq_len, use_t=train_config.use_t,
             invert=model_name.startswith('Inverted'),
             params={
                 'deeponet_hidden_size': model_config.deeponet_hidden_size,
